da4294ce5c59669f9a69a5ff7ad536a92b4bfc6c
HikaroRibeiro <hhakawama@gmail.com>
2025-03-23 17:19:54 -0300

    ✨ Cabeçalho Estilizado
    Cabeçalho com estile pre-definido.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,11 @@
 // import * as S from './styles' Outra forma de importação dos componentes.
+import { Header } from '@components/Header'
 import { Container, Title } from './styles'
 
 export function Groups() {
   return (
     <Container>
+      <Header />
       <Title>Groups</Title>
     </Container>
   )
