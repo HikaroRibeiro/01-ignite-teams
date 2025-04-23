20dbfdedbce738b42153e5c080f740146d9a8820
HikaroRibeiro <hhakawama@gmail.com>
2025-04-23 11:26:45 -0300

    ✨ Componente NewGroup
    Criado o componente estilizado NewGroup.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,21 +6,28 @@ import { Container, Title } from './styles'
 import { GroupCard } from '@components/GroupCard'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
+import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Rocketseat', 'Galera Dev'])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
       <Header />
       <Title>Groups</Title>
-      <Highlight title="Turma 1" subtitle="Jogue com sua turma"></Highlight>
+      <Highlight title="Turma 1" subtitle="Jogue com sua turma" />
 
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
+      <Button title="Criar nova turma"></Button>
     </Container>
   )
 }
